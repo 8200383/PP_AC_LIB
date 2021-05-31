a4d587a32b6eaa19ad2e28666b98bc589d4af967
aimproxy <diasmicaelandre@gmail.com>
2021-05-31 20:51:06 +0100

    Bootstrap Project

---
+++ src/Monitoring/Station.java
@@ -0,0 +1,14 @@
+package Monitoring;
+
+/*
+ * Nome: Micael André Cunha Dias
+ * Número: 8200383
+ * Turma: LEI1T4
+ *
+ * Nome: Hugo Henrique Almeida Carvalho
+ * Número: 8200590
+ * Turma: LEI1T3
+ */
+
+public class Station {
+}
