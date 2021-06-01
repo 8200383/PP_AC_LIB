f528a8f20b6a5e6d104cc846c2407803fe4027eb
aimproxy <diasmicaelandre@gmail.com>
2021-06-01 22:46:29 +0100

    Remove Custom Array

--- src/MA02/Main.java
+++ src/MA02/Main.java
@@ -1,9 +1,5 @@
 package MA02;
 
-
-import Utils.CustomArray;
-import Monitoring.SensorFactory.Sensor;
-import Utils.ICustomArray;
 import edu.ma02.core.exceptions.MeasurementException;
 import edu.ma02.core.exceptions.SensorException;
 
@@ -18,22 +14,5 @@ public class Main {
                 38.70263097,
                 -9.199692206
         ); */
-
-        ICustomArray array = new CustomArray(10);
-        Sensor[] sensors = (Sensor[]) array.getAll();
-
-        for (Sensor o : sensors) {
-            System.out.println(o.getType());
-        }
-
-        // Alternativa
-        CustomArray arr = new CustomArray();
-        for (Object o : arr.objects) {
-            if (o instanceof Sensor) {
-                Sensor s = (Sensor) o;
-                System.out.println(s.getType());
-            }
-        }
-
     }
 }
