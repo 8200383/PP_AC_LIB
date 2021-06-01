1b84304291564591074a89ad204532067dc0b614
aimproxy <diasmicaelandre@gmail.com>
2021-06-01 22:29:23 +0100

    Comment Demo Sensor Factory

--- src/MA02/Main.java
+++ src/MA02/Main.java
@@ -10,14 +10,14 @@ import edu.ma02.core.exceptions.SensorException;
 public class Main {
     public static void main(String[] args) throws SensorException, MeasurementException {
 
-        Sensor sensor = Sensor.SensorFactory(
+       /*  Sensor sensor = Sensor.SensorFactory(
                 "ME00PA0001",
                 -92781,
                 -106663,
                 0,
                 38.70263097,
                 -9.199692206
-        );
+        ); */
 
         ICustomArray array = new CustomArray(10);
         Sensor[] sensors = (Sensor[]) array.getAll();
