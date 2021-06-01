ad373794a83feba063d326c8a07c16c590eee481
aimproxy <diasmicaelandre@gmail.com>
2021-06-01 11:07:59 +0100

    Add Sensor Exceptions

---
+++ src/Monitoring/Sensors/Exceptions/UnrecognizedSensorUnit.java
@@ -0,0 +1,19 @@
+package Monitoring.Sensors.Exceptions;
+
+import edu.ma02.core.exceptions.SensorException;
+
+/*
+ * Nome: Micael André Cunha Dias
+ * Número: 8200383
+ * Turma: LEI1T4
+ *
+ * Nome: Hugo Henrique Almeida Carvalho
+ * Número: 8200590
+ * Turma: LEI1T3
+ */
+public class UnrecognizedSensorUnit extends SensorException {
+
+    public UnrecognizedSensorUnit() {
+        super("Unrecognized Sensor Parameter");
+    }
+}
