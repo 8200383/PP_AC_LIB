952bbb8c5daa30c407e92a4628a45c0fc9667e9c
aimproxy <diasmicaelandre@gmail.com>
2021-06-01 11:08:46 +0100

    Add Sensor ID VO

---
+++ src/Monitoring/Sensors/ValueObjects/SensorId.java
@@ -0,0 +1,27 @@
+package Monitoring.Sensors.ValueObjects;
+
+import edu.ma02.core.exceptions.SensorException;
+
+/*
+ * Nome: Micael André Cunha Dias
+ * Número: 8200383
+ * Turma: LEI1T4
+ *
+ * Nome: Hugo Henrique Almeida Carvalho
+ * Número: 8200590
+ * Turma: LEI1T3
+ */
+public class SensorId {
+    private final String sensorId;
+
+    public SensorId(String id) throws SensorException {
+        if (id.length() != 10) throw new SensorException("Invalid Sensor Id Length");
+
+        sensorId = id;
+    }
+
+    @Override
+    public String toString() {
+        return sensorId;
+    }
+}
