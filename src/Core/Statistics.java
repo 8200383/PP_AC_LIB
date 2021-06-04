95679c69991c34bee67578cd10c4ea44636e5944
aimproxy <diasmicaelandre@gmail.com>
2021-06-04 09:10:40 +0100

    Add a class Statistics

---
+++ src/Core/Statistics.java
@@ -0,0 +1,24 @@
+package Core;
+
+import edu.ma02.core.interfaces.IStatistics;
+
+/*
+ * Nome: Micael André Cunha Dias
+ * Número: 8200383
+ * Turma: LEI1T4
+ *
+ * Nome: Hugo Henrique Almeida Carvalho
+ * Número: 8200590
+ * Turma: LEI1T3
+ */
+public class Statistics implements IStatistics {
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public double getValue() {
+        return 0;
+    }
+}
